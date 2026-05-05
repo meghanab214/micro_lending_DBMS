496f5007835b2b6623853c3ac5267025363aa0d0
Supraa-30 <suprajnah.eai24@chanakyauniversity.edu.in>
2026-05-05 16:34:08 +0530

    updated meg's push

--- backend/app/config/db.py
+++ backend/app/config/db.py
@@ -4,7 +4,7 @@ def get_connection():
     return psycopg2.connect(
         dbname="micro_lending",
         user="postgres",
-        password="yourpassword",
+        password="Megh@na14",
         host="localhost",
         port="5432"
     )
