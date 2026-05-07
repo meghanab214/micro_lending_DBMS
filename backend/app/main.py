29a819888b0f25ad9853fd412617b67e8f547384
Supraa-30 <suprajnah.eai24@chanakyauniversity.edu.in>
2026-05-07 15:36:59 +0530

    Backend done

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from app.routes.investor_routes import router as investor_router
 from app.routes.repayment_routes import router as repayment_router
 from app.routes.repayment_routes import router as kyc_router
+from app.routes.loan_routes import router as loan_router
 
 app = FastAPI()
 
 app.include_router(investor_router)
 app.include_router(repayment_router)
 app.include_router(kyc_router)
+app.include_router(loan_router)  
